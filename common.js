f6ace74820d7057ec16592191f6264ee9702d209
Aleksander Malinowski <olekmali@ieee.org>
2022-03-20 14:51:07 -0500

    Initial setup of the project based on Jason's Smart Office Calendar. See https://github.com/jason1813/Smart-Calendar

---
+++ common.js
@@ -0,0 +1,32 @@
+var IDLE_TIMEOUT = 90; //seconds
+var _idleSecondsCounter = 0;
+
+document.onclick = function()
+{
+    _idleSecondsCounter = 0;
+};
+
+document.onmousemove = function()
+{
+    _idleSecondsCounter = 0;
+};
+
+document.onkeypress = function()
+{
+    _idleSecondsCounter = 0;
+};
+
+function CheckIdleTime()
+{
+    if ( _idleSecondsCounter < IDLE_TIMEOUT)
+    {
+        _idleSecondsCounter++;
+    } else {
+        window.location.replace("index.html");
+    }
+}
+
+function startTimeout()
+{
+    window.setInterval(CheckIdleTime, 1000);
+}
