8bc4127533e9084c3f0a535fc17bba2064d333e7
Aleksander Malinowski <olekmali@ieee.org>
2022-03-24 23:56:36 -0500

    modified reload to affect only iframe page or image

--- common.js
+++ common.js
@@ -1,33 +1,21 @@
-var IDLE_TIMEOUT = 3600; //seconds
-var _idleSecondsCounter = 0;
+var TIME_FOR_ACTION = 3600; //seconds
+var _SecondsCounter = 0;
 
-document.onclick = function()
+function CheckTime()
 {
-    _idleSecondsCounter = 0;
-};
-
-document.onmousemove = function()
-{
-    _idleSecondsCounter = 0;
-};
-
-document.onkeypress = function()
-{
-    _idleSecondsCounter = 0;
-};
-
-function CheckIdleTime()
-{
-    if ( _idleSecondsCounter < IDLE_TIMEOUT)
+    if ( _SecondsCounter < TIME_FOR_ACTION)
     {
-        _idleSecondsCounter++;
+        _SecondsCounter++;
     } else {
+        _SecondsCounter = 0;
         // window.location.replace("index.html"); // return to home location
-        window.location.reload(); // reload the current page
+        // window.location.reload(); // reload the current page
+        var toreload = document.getElementById('refreshable');
+        toreload.src = toreload.src;
     }
 }
 
 function startTimeout()
 {
-    window.setInterval(CheckIdleTime, 1000);
+    window.setInterval(CheckTime, 1000);
 }
